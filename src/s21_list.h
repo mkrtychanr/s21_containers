3572c68709199ea970108132adcc4e406d35de0e
Maelys Tiffaney <maelysti@at-n6.msk.21-school.ru>
2023-01-12 16:57:44 +0300

    almost done

--- src/s21_list.h
+++ src/s21_list.h
@@ -1,58 +1,235 @@
 #ifndef CPP2_S21_CONTAINERS_SRC_S21_LIST_H_
 #define CPP2_S21_CONTAINERS_SRC_S21_LIST_H_
 
+#include <cstddef>
+#include <cstdio>
+#include <iostream>
+
 template <typename T>
-class list<T> {
+class list {
+ private:
+  struct ListNode;
+
  public:
-  typedef T value_type;
-  typedef value_type& reference;
-  typedef const value_type& const_reference;
-  typedef ListIterator<value_type> iterator;
-  typedef ListConstIterator<value_type> const_iterator;
-  typedef size_t size_type;
+  class ListIterator;
+  class ListConstIterator;
 
-  class ListIterator<value_type> {};
-  class ListConstIterator<value_type> {};
+  using value_type = T;
+  using reference = value_type&;
+  using const_reference = const value_type&;
+  using iterator = ListIterator;
+  using const_iterator =  const ListConstIterator;
+  using size_type = size_t;
+  using pointer = ListNode*;
 
-  list();
+  class ListIterator {
+    friend class list;
+   public:
+    ListIterator() : node_(ListNode()) {}
 
-  list(size_type n);
+    ListIterator(const iterator& other) { operator=(other); }
 
-  list(std::initalizer_list<value_type> const &items);
+    iterator& operator=(const iterator& other) {
+      node_ = other.node_;
+      return *this;
+    }
+    
+    iterator& operator++() { 
+      node_ = node_ -> next_;
+      return *this;
+    }
 
-  list(const list& l);
+    iterator& operator--() {
+      node_ = node_ -> previous_;
+      return *this;
+    }
 
-  list(list&& l);
+    reference operator*() { return node_ -> value_; }
 
-  ~list();
+    friend bool operator==(const iterator& lhs, const iterator& rhs) {
+      return lhs.node_ == rhs.node_;
+    }
 
-  list& operator=(list &&l);
+    friend bool operator!=(const iterator& lhs, const iterator& rhs) {
+      return !(lhs == rhs);
+    }
 
-  list& operator=(cosnt list& l);
+   private:
 
-  const_reference front() const;
+    ListIterator(const pointer node) : node_(node) {}
 
-  const_reference back() const;
+    pointer node_;
+  };
 
-  iterator begin();
+  class ListConstIterator {
+    friend class list;
+   public:
+    ListConstIterator() : node_(ListNode()) {}
 
-  iterator end();
+    ListConstIterator(const_iterator& other) { operator=(other); }
 
-  bool empty() const;
+    const_iterator& operator=(const_iterator& other) {
+      node_ = other.node_;
+      return *this;
+    }
+    
+    const_iterator& operator++() { 
+      node_ = node_ -> next_;
+      return *this;
+    }
 
-  size_type size() const;
+    const_iterator& operator--() {
+      node_ = node_ -> previous_;
+      return *this;
+    }
+
+    const_reference operator*() const { return node_ -> value_; }
+
+    friend bool operator==(const_iterator& lhs, const_iterator& rhs) {
+      return lhs.node_ == rhs.node_;
+    }
+
+    friend bool operator!=(const_iterator& lhs, const_iterator& rhs) {
+      return !(lhs == rhs);
+    }
+
+   private:
+
+    ListConstIterator(const pointer node) : node_(node) {}
+
+    pointer node_;
+  };
+
+  list() {
+    clear();
+    first_ = nullptr;
+    last_ = nullptr;
+    size_ = 0;
+  }
+
+  list(size_type n) {
+    list();
+    for (size_type i = 0; i < n; i++) {
+      push_back(value_type());
+    }
+  }
+
+  // list(std::initalizer_list<value_type> const &items);
+
+  list(const list& l) {
+    operator=(l);
+  }
+
+  list(list&& l) {
+    operator=(l);
+  }
+
+  ~list() {
+    clear();
+  }
+
+  list& operator=(list &&l) {
+    if (this != &l) {
+      this -> first_ = l.first_;
+      this -> last_ = l.last_;
+      this -> size_ = l.size_;
+      l.first_ = nullptr;
+      l.last_ = nullptr;
+      l.size_ = 0;
+    }
+    return *this;
+  }
+
+  list& operator=(const list& l) {
+    clear();
+    for (auto it = l.begin(); it != l.end(); ++it) {
+      push_back(*it);
+    }
+    return *this;
+  }
+
+  const_reference front() const {
+    if (first_ == nullptr) {
+      return value_type();
+    }
+    return *first_;
+  }
+
+  const_reference back() const {
+    if (last_ == nullptr) {
+      return value_type();
+    }
+    return *last_;
+  }
+
+  iterator begin() {
+    return iterator(first_);
+  }
+
+  iterator end() {
+    return iterator(nullptr);
+  }
+
+  const_iterator begin() const {
+    return const_iterator(first_);
+  }
+
+  const_iterator end() const {
+    return const_iterator(last_);
+  }
+
+  bool empty() const { return size_ == 0; }
+
+  size_type size() const { return size_; }
 
   size_type max_size() const;
 
-  void clear();
+  void clear() {
+    while (!empty()) {
+      pop_back();
+    }
+  }
 
   iterator insert(iterator pos, const_reference value);
 
-  void erase(iterator pos);
+  void erase(iterator pos) {
+    if (pos.node_ != nullptr) {
+      auto newNextForPrevious = pos.node_ -> next_;
+      auto newPreviousForNext = pos.node_ -> previous_;
+      if (pos.node_ -> previous_ != nullptr) {
+        pos.node_ -> previous_ -> next_ = newNextForPrevious;
+      }
+      if (pos.node_ -> next_ != nullptr) {
+        pos.node_ -> next_ -> previous_ = newPreviousForNext;
+      }
+      delete pos.node_;
+    }
+  }
 
-  void push_back(const_reference value);
+  void push_back(const_reference value) {
+    if (last_ == nullptr) {
+      last_ = new ListNode(value);
+      first_ = last_;
+    } else {
+      last_ -> next_ = new ListNode(value, last_);
+      last_ = last_ -> next_;
+    }
+    size_++;
+  }
 
-  void pop_back();
+  void pop_back() {
+    if (last_ != nullptr) {
+      auto toDelete = last_;
+      if (last_ -> previous_ == nullptr) {
+        first_ = nullptr;
+      } else {
+        last_ = last_ -> previous_;
+        last_ -> next_ = nullptr;
+      }
+      erase(iterator(toDelete));
+      size_--;
+    }
+  }
 
   void push_front(const_reference value);
 
@@ -71,10 +248,18 @@ class list<T> {
   void sort();
 
  private:
-  struct ListNode<value_type> {
-    value_type value = value_type();
-    ListNode* previous = nullptr;
-    ListNode* next = nullptr;
+
+  pointer first_ = nullptr;
+
+  pointer last_ = nullptr;
+
+  size_type size_ = 0;
+
+  struct ListNode {
+    value_type value_ = value_type();
+    ListNode* previous_ = nullptr;
+    ListNode* next_ = nullptr;
+    ListNode(value_type value = value_type(), ListNode* previous = nullptr, ListNode* next = nullptr) : value_(value), previous_(previous), next_(next) {}
   };
 };
 
