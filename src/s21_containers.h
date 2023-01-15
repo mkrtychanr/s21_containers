0a19bbae5a6a4f7d8a517207fde8abd77c8a6718
Maelys Tiffaney <maelysti@student.21-school.ru>
2023-01-15 18:24:10 +0300

    add lib files

---
+++ src/s21_containers.h
@@ -0,0 +1,11 @@
+#ifndef CPP2_S21_CONTAINERS_SRC_S21_CONTAINERS_H_
+#define CPP2_S21_CONTAINERS_SRC_S21_CONTAINERS_H_
+
+#include "headers/s21_list.h"
+#include "headers/s21_queue.h"
+#include "headers/s21_stack.h"
+#include "headers/s21_set.h"
+#include "headers/s21_map.h"
+#include "headers/s21_vector.h"
+
+#endif  // CPP2_S21_CONTAINERS_SRC_S21_CONTAINERS_H_
